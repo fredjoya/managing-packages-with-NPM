83183f81fd966091af12971e97078256cbbe4fd2
Your Name <ferdousjoya>
2021-10-24 09:46:09 -0700

    changes

--- public/myApp.js
+++ public/myApp.js
@@ -1,17 +1,16 @@
 var express = require('express');
 var app = express();
 var bGround = require('fcc-express-bground');
-
-
-
-
-
-
-
+var myApp = require('./myApp');
 
 
 bGround.log("Hello World");
-console.log("Hello World");
+
+
+
+
+
+
 
 
 
