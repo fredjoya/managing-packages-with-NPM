7a1dc0a34f2ae8233151248e2c40f16a1b36ad03
Your Name <ferdousjoya>
2021-10-24 13:27:43 -0700

    changes

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var bGround = require('fcc-express-bground');
-
+require('dotenv').config();
 
 
 
@@ -25,6 +25,19 @@ app.get("/json", (req, res) => {
     res.json(
         {"message": "Hello json"}
     );
+});
+
+app.get("/json", function(req, res) {
+    console.log(process.env.MESSAGE_STYLE, " <= message style");
+    if (process.env.MESSAGE_STYLE === uppercase) {
+        res.json(
+            { "message": "HELLO JSON" }
+        )
+    } else {
+        res.json(
+            { "message": "Hello json" }
+        )
+    }
 });
 
 
