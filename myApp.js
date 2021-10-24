677c9df91d897ab1bbf997532bf3bd7677d72d7d
Your Name <ferdousjoya>
2021-10-24 13:53:32 -0700

    changes

--- myApp.js
+++ myApp.js
@@ -21,25 +21,20 @@ app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
 
 
-app.get("/json", (req, res) => {
-    res.json(
-        {"message": "Hello json"}
-    );
-});
+
 
 app.get("/json", function(req, res) {
     console.log(process.env.MESSAGE_STYLE, " <= message style");
     if (process.env.MESSAGE_STYLE === "uppercase") {
         res.json(
-            { "message": "HELLO JSON" }
+            {"message": "HELLO JSON"}
         )
     } else {
         res.json(
-            { "message": "Hello json" }
+            {"message": "Hello json"}
         )
     }
 });
-
 
 
 
