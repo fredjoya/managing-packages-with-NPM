e97a171457f00355798aa757802228cda6091807
Your Name <ferdousjoya>
2021-10-24 12:35:17 -0700

    add json data

--- myApp.js
+++ myApp.js
@@ -21,7 +21,11 @@ app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
 
 
-
+app.get("/json", function(req, res) {
+    res.json(
+        {"message": "Hello json"}
+    );
+});
 
 
 
